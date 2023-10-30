ffcdad7babad822f50edabf1b6e756f2a185f03a
nilam2806 <112157767+nilam2806@users.noreply.github.com>
2023-10-30 22:24:09 +0530

    Delete src directory

--- src/com/example/exercises/DirectorGenresPair.java
+++
@@ -1,9 +0,0 @@
-package com.example.exercises;
-
-import java.util.List;
-
-import com.example.domain.Director;
-import com.example.domain.Genre;
-
-
-record DirectorGenresPair(Director director,List<Genre> genres) { }
