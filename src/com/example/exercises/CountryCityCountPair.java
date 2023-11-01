cb3a66eabef7428fcbf8158c346fad3e7243d885
nilam2806 <112157767+nilam2806@users.noreply.github.com>
2023-11-01 12:29:59 +0530

    Delete src directory

--- src/com/example/exercises/CountryCityCountPair.java
+++
@@ -1,6 +0,0 @@
-package com.example.exercises;
-
-import com.example.domain.Country;
-
-
-public record CountryCityCountPair(Country country,int count) { }
--- src/com/example/exercises/DirectorGenrePair.java
+++
@@ -1,6 +0,0 @@
-package com.example.exercises;
-
-import com.example.domain.Director;
-import com.example.domain.Genre;
-
-record DirectorGenrePair(Director director,Genre genre) { }
